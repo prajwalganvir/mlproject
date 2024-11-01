cff2ae54fa59c98d52fe10721b7caaf85a4cdf55
prajwalganvir <prajwalganvir001@gmail.com>
2024-11-01 13:00:30 +0530

    adding notebook files

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -18,4 +18,9 @@ class CustomException(Exception):
         return self.error_message
     
     
-
+    
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         raise CustomException(e,sys)
